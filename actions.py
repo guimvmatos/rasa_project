a803e137e1ae079bec873376ad6e0f39ff10b37f
Guilherme Matos <guimvmatos1@outlook.com>
2021-04-14 16:46:13 -0300

    update

--- actions.py
+++ actions.py
@@ -1,11 +1,17 @@
 from typing import Any, Text, Dict, List, Union
 
+import requests
+import json
+
 from requests.exceptions import RequestException
+from requests.structures import CaseInsensitiveDict
 from requests.models import Response
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormAction
+
+
 
 class AlunoForm(FormAction):
 
@@ -15,7 +21,7 @@ class AlunoForm(FormAction):
     @staticmethod
     def required_slots(tracker):
 
-        return ["ra"]
+        return ["ra", "nome"]
        
     def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
         return {
@@ -26,6 +32,28 @@ class AlunoForm(FormAction):
             ],
         }
 
+
+    def buscarDadosAluno(self,ra_aluno):
+        request_url=f"http://api.feb.br/RM_Controller_Central/getAluno"
+    
+        headers = CaseInsensitiveDict()
+        headers["Content-Type"] = "application/x-www-form-urlencoded"
+        headers["Accept"] = "application/json"
+        
+        data = "ra=%s" % (ra_aluno)
+        response = requests.post(
+                request_url, headers=headers, data=data
+            )
+        
+        reply=response.json()
+        raf=reply['ra']
+        nomef=reply['nome_completo']
+        celularf=reply['celular']
+        emailf=reply['email']
+
+        return raf,nomef,emailf
+    
+
     def submit(
         self,
         dispatcher: CollectingDispatcher,
@@ -33,5 +61,7 @@ class AlunoForm(FormAction):
         domain: Dict[Text, Any],
     ) -> List[Dict]:
 
-        dispatcher.utter_message("Obrigado pelas informações")
+        data=self.buscarDadosAluno(512203)
+        
+        dispatcher.utter_message("Obrigado pelas informações %s" % data[1])
         return []
