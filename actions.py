03fc9fe72ec015b3744ee3c58e71df44090c814b
Guilherme Matos <guimvmatos1@outlook.com>
2021-05-21 13:47:31 -0300

    new

--- actions.py
+++ actions.py
@@ -106,7 +106,7 @@ class handoverAction(Action):
             "action": "handover",
             "sessionId": tracker.sender_id,
             "actionData": {
-              "targetDepartment": "Test Bot2"
+              "targetDepartment": "Test Bot3"
             }
         }
         
